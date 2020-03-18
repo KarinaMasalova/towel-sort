e57e36568d007f9b9743f17c8c218eea8597f1b4
Karina Masalova <masalova.karina@mail.ru>
2020-03-18 22:26:02 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let isReversed = false;
+  let newArr = [];
+
+  if (!matrix || matrix.length == 0) return [];
+
+  if (matrix.length == 1) return matrix;
+
+  matrix.forEach(function (row) {
+    if(!isReversed) {
+        row.forEach(item => newArr.push(item));
+        isReversed = true;
+    } else {
+        row.reverse().forEach(item => newArr.push(item));
+        isReversed = false;
+    }
+  });  
+
+  return newArr;
 }
